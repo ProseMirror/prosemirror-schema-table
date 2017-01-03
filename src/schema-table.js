ecb55bf3b58d347720f648484c8d7facec6a8021
Marijn Haverbeke <marijn@haverbeke.nl>
2017-01-03 08:57:09 +0100

    Move from actions to transactions

--- src/schema-table.js
+++ src/schema-table.js
@@ -280,90 +280,90 @@ function findRow($pos, pred) {
   return -1
 }
 
-// :: (EditorState, onAction: ?(action: Action)) → bool
+// :: (EditorState, dispatch: ?(tr: Transaction)) → bool
 // Command function that adds a column before the column with the
 // selection.
-function addColumnBefore(state, onAction) {
+function addColumnBefore(state, dispatch) {
   let $from = state.selection.$from, cellFrom
   let rowDepth = findRow($from, d => cellFrom = d == $from.depth ? $from.nodeBefore : $from.node(d + 1))
   if (rowDepth == -1) return false
-  if (onAction)
-    onAction(state.tr.step(AddColumnStep.create(state.doc, $from.before(rowDepth - 1), $from.index(rowDepth),
-                                                cellFrom.type, cellFrom.attrs)).action())
+  if (dispatch)
+    dispatch(state.tr.step(AddColumnStep.create(state.doc, $from.before(rowDepth - 1), $from.index(rowDepth),
+                                                cellFrom.type, cellFrom.attrs)))
   return true
 }
 exports.addColumnBefore = addColumnBefore
 
-// :: (EditorState, onAction: ?(action: Action)) → bool
+// :: (EditorState, dispatch: ?(tr: Transaction)) → bool
 // Command function that adds a column after the column with the
 // selection.
-function addColumnAfter(state, onAction) {
+function addColumnAfter(state, dispatch) {
   let $from = state.selection.$from, cellFrom
   let rowDepth = findRow($from, d => cellFrom = d == $from.depth ? $from.nodeAfter : $from.node(d + 1))
   if (rowDepth == -1) return false
-  if (onAction)
-    onAction(state.tr.step(AddColumnStep.create(state.doc, $from.before(rowDepth - 1),
+  if (dispatch)
+    dispatch(state.tr.step(AddColumnStep.create(state.doc, $from.before(rowDepth - 1),
                                                 $from.indexAfter(rowDepth) + (rowDepth == $from.depth ? 1 : 0),
-                                                cellFrom.type, cellFrom.attrs)).action())
+                                                cellFrom.type, cellFrom.attrs)))
   return true
 }
 exports.addColumnAfter = addColumnAfter
 
-// :: (EditorState, onAction: ?(action: Action)) → bool
+// :: (EditorState, dispatch: ?(tr: Transaction)) → bool
 // Command function that removes the column with the selection.
-function removeColumn(state, onAction) {
+function removeColumn(state, dispatch) {
   let $from = state.selection.$from
   let rowDepth = findRow($from, d => $from.node(d).childCount > 1)
   if (rowDepth == -1) return false
-  if (onAction)
-    onAction(state.tr.step(RemoveColumnStep.create(state.doc, $from.before(rowDepth - 1), $from.index(rowDepth))).action())
+  if (dispatch)
+    dispatch(state.tr.step(RemoveColumnStep.create(state.doc, $from.before(rowDepth - 1), $from.index(rowDepth))))
   return true
 }
 exports.removeColumn = removeColumn
 
-function addRow(state, onAction, side) {
+function addRow(state, dispatch, side) {
   let $from = state.selection.$from
   let rowDepth = findRow($from)
   if (rowDepth == -1) return false
-  if (onAction) {
+  if (dispatch) {
     let exampleRow = $from.node(rowDepth)
     let cells = [], pos = side < 0 ? $from.before(rowDepth) : $from.after(rowDepth)
     exampleRow.forEach(cell => cells.push(cell.type.createAndFill(cell.attrs)))
     let row = exampleRow.copy(Fragment.from(cells))
-    onAction(state.tr.step(new ReplaceStep(pos, pos, new Slice(Fragment.from(row), 0, 0))).action())
+    dispatch(state.tr.step(new ReplaceStep(pos, pos, new Slice(Fragment.from(row), 0, 0))))
   }
   return true
 }
 
-// :: (EditorState, onAction: ?(action: Action)) → bool
+// :: (EditorState, dispatch: ?(tr: Transaction)) → bool
 // Command function that adds a row after the row with the
 // selection.
-function addRowBefore(state, onAction) {
-  return addRow(state, onAction, -1)
+function addRowBefore(state, dispatch) {
+  return addRow(state, dispatch, -1)
 }
 exports.addRowBefore = addRowBefore
 
-// :: (EditorState, onAction: ?(action: Action)) → bool
+// :: (EditorState, dispatch: ?(tr: Transaction)) → bool
 // Command function that adds a row before the row with the
 // selection.
-function addRowAfter(state, onAction) {
-  return addRow(state, onAction, 1)
+function addRowAfter(state, dispatch) {
+  return addRow(state, dispatch, 1)
 }
 exports.addRowAfter = addRowAfter
 
-// :: (EditorState, onAction: ?(action: Action)) → bool
+// :: (EditorState, dispatch: ?(tr: Transaction)) → bool
 // Command function that removes the row with the selection.
-function removeRow(state, onAction) {
+function removeRow(state, dispatch) {
   let $from = state.selection.$from
   let rowDepth = findRow($from, d => $from.node(d - 1).childCount > 1)
   if (rowDepth == -1) return false
-  if (onAction)
-    onAction(state.tr.step(new ReplaceStep($from.before(rowDepth), $from.after(rowDepth), Slice.empty)).action())
+  if (dispatch)
+    dispatch(state.tr.step(new ReplaceStep($from.before(rowDepth), $from.after(rowDepth), Slice.empty)))
   return true
 }
 exports.removeRow = removeRow
 
-function moveCell(state, dir, onAction) {
+function moveCell(state, dir, dispatch) {
   let {$from} = state.selection
   let rowDepth = findRow($from)
   if (rowDepth == -1) return false
@@ -372,7 +372,7 @@ function moveCell(state, dir, onAction) {
     let $cellStart = state.doc.resolve(row.content.offsetAt(newIndex) + $from.start(rowDepth))
     let sel = Selection.findFrom($cellStart, 1)
     if (!sel || sel.from >= $cellStart.end()) return false
-    if (onAction) onAction(sel.scrollAction())
+    if (dispatch) dispatch(state.tr.setSelection(sel).scrollIntoView())
     return true
   } else {
     let rowIndex = $from.index(rowDepth - 1) + dir, table = $from.node(rowDepth - 1)
@@ -380,17 +380,17 @@ function moveCell(state, dir, onAction) {
     let cellStart = dir > 0 ? $from.after(rowDepth) + 2 : $from.before(rowDepth) - 2 - table.child(rowIndex).lastChild.content.size
     let $cellStart = state.doc.resolve(cellStart), sel = Selection.findFrom($cellStart, 1)
     if (!sel || sel.from >= $cellStart.end()) return false
-    if (onAction) onAction(sel.scrollAction())
+    if (dispatch) dispatch(state.tr.setSelection(sel).scrollIntoView())
     return true
   }
 }
 
-// :: (EditorState, onAction: ?(action: Action)) → bool
+// :: (EditorState, dispatch: ?(tr: Transaction)) → bool
 // Move to the next cell in the current table, if there is one.
-function selectNextCell(state, onAction) { return moveCell(state, 1, onAction) }
+function selectNextCell(state, dispatch) { return moveCell(state, 1, dispatch) }
 exports.selectNextCell = selectNextCell
 
-// :: (EditorState, onAction: ?(action: Action)) → bool
+// :: (EditorState, dispatch: ?(tr: Transaction)) → bool
 // Move to the previous cell in the current table, if there is one.
-function selectPreviousCell(state, onAction) { return moveCell(state, -1, onAction) }
+function selectPreviousCell(state, dispatch) { return moveCell(state, -1, dispatch) }
 exports.selectPreviousCell = selectPreviousCell
